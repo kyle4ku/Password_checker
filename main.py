42d1a62e4e73e81c2a9ae14178612fe38fa00ae6
kyle4ku <umeozulukennet@gmail.com>
2024-05-17 14:34:08 +0100

    make way

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+import hashlib
+import sys
+
+
+def request(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    rep = requests.get(url)
+    if rep.status_code != 200:
+        raise RuntimeError(f'Error fetching {rep.status_code}, please try again')
+    return rep
+
+
+def get_password_leak_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for hashed, count in hashes:
+        if hashed == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    hashed_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first_hashed_5, remaining = hashed_password[:5], hashed_password[5:]
+    response = request(first_hashed_5)
+    print(first_hashed_5)
+    return get_password_leak_count(response, remaining)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'Your {password} was found {count} times....you should change your password')
+        else:
+            print(f'Your {password} is safe')
+    return 'done'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
